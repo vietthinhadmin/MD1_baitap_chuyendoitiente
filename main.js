eeeed201f49cbb024832de22d91c1849a9df5c26
Viết Thịnh <vietthinh.bacninh@gmail.com>
2022-12-07 10:17:41 +0700

    ifelse

--- main.js
+++
@@ -1,5 +0,0 @@
-function exchangeCurrency() {
-    let vnd = + document.getElementById('amount').value;
-    let usd = vnd / 23000;
-    document.getElementById('result').innerHTML  = 'Result: ' + usd
-}
